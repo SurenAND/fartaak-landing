18dad722ec0e16233aa29a68b8a37a9dc7d83613
AshkanZjj <140895184+SurenAND@users.noreply.github.com>
2024-08-21 19:33:30 +0330

    feat: create services section

--- src/components/templates/Home/HomeTemplate.tsx
+++ src/components/templates/Home/HomeTemplate.tsx
@@ -1,13 +1,17 @@
 import AboutUs from "@/src/components/templates/Home/about-us/AboutUs";
 import Hero from "@/src/components/templates/Home/hero/Hero";
+import Services from "@/src/components/templates/Home/services/Services";
 import Universities from "@/src/components/templates/Home/universities/Universities";
 
 const HomeTemplate = () => {
-  return <>
-    <Hero />
-    <AboutUs />
-    <Universities />
-  </>;
+  return (
+    <>
+      <Hero />
+      <AboutUs />
+      <Universities />
+      <Services />
+    </>
+  );
 };
 
 export default HomeTemplate;
---
+++ src/components/templates/Home/services/data.ts
@@ -0,0 +1,24 @@
+import Service2 from "@/src/assets/images/services/education.svg";
+import Service1 from "@/src/assets/images/services/fund.svg";
+import Service3 from "@/src/assets/images/services/support.svg";
+
+export const services = [
+  {
+    src: Service1,
+    alt: "Service1",
+    text: "بورس تحصیلی",
+    desc: "ثبت درخواست و پیگیری مراحل بورسیه",
+  },
+  {
+    src: Service2,
+    alt: "Service2",
+    text: "مشاوره تحصیلی",
+    desc: "ویزای تحصیلی انواع رشته و مقاطع بالاتر",
+  },
+  {
+    src: Service3,
+    alt: "Service3",
+    text: "پشتیبانی",
+    desc: "انجام تمامی مراحل بورسیه",
+  },
+];
