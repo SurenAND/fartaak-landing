9d7748a47a06d75b50a3cbc6d2cd5ef01f1a3398
AshkanZjj <140895184+SurenAND@users.noreply.github.com>
2024-08-21 19:04:34 +0330

    feat: create about us section

--- src/components/templates/Home/HomeTemplate.tsx
+++ src/components/templates/Home/HomeTemplate.tsx
@@ -1,7 +1,11 @@
+import AboutUs from "@/src/components/templates/Home/about-us/AboutUs";
 import Hero from "@/src/components/templates/Home/hero/Hero";
 
 const HomeTemplate = () => {
-  return <Hero/>;
+  return <>
+    <Hero />
+    <AboutUs />
+  </>;
 };
 
 export default HomeTemplate;
